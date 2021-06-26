a533bb1fac9c869dd2aa5bce5ee09183188ef8d0
ivy118 <47249996+ivy118@users.noreply.github.com>
2021-06-26 00:39:38 -0400

    jnvfjnjbh

---
+++ server/server.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require('morgan');
+require('dotenv').config();
+const db = require('./db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('../client/public'));
+
+
+
+//home page: need boxes name and boxes image to render on page!
+app.get('/home', async (req, res) => {
+    try{
+        const results = await db.query("select * from boxes;");
+        res.status(200).json({
+            status: "success",     
+            boxes: results.rows,
+        });
+    } catch(err) {
+        console.log('Error found in get method to /home',err); 
+    }
+});
+
+//signup page: needs to store user info in database, NEED TO BE FIXED
+
+app.get('/signup', async (req, res, next) => {
+    try{
+        const results = await db.query("select * from users WHERE ;");
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            user: results.rows,
+        });
+    } catch(err) {
+        console.log(err); 
+    }
+});
+
+
+app.post('/signup', async (req, res) => {
+    try{
+        const results = await db.query("INSERT INTO users (firstname, lastname, username, pass, email) values ($1, $2, $3, $4, $5) returning *;", [req.body.firstname, req.body.lastname, req.body.username, req.body.pass, req.body.email]);
+        res.status(200).json({
+            status: "success",     
+            user: results.rows[0],
+        });
+    } catch(err) {
+        console.log('Error found in get method to /signup',err); 
+    }
+});
+
+
+//login page: need to check that the user's email and password is correct???
+app.get('/login', async (req, res) => {
+    try{
+        const results = await db.query("select * from users;");
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                restaurant: results.rows,
+            },
+        });
+    } catch(err) {
+        console.log(err); 
+    }
+});
+
+app.get('/restaurant/:id', async (req, res) => {
+    try {
+        const restaurant = await db.query("select * from restaurant where id = $1;", [req.params.id]);
+
+        const reviews = await db.query("select * from review where restaurant_id = $1;", [req.params.id]);
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: restaurant.rows[0],
+                reviews: reviews.rows,
+            },
+        });
+    } catch(err) {
+        console.log(err);
+    }
+});
+
+
+//to get japanese box and its items to show up on the page, when you click on it!
+app.get('home/:japanese-box', async (req, res) => {
+    try{
+        const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.japaness-box]);
+        res.status(200).json({
+            status: "success",     
+            items: results.rows,
+        });
+    } catch(err) {
+        console.log('Error found in get method to home/japanese-box', err); 
+    }
+});
+
+//to get korean box and its items to show up on the page, when you click on it!
+app.get('home/:korean-box', async (req, res) => {
+    try{
+        const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.korean-box]);
+        res.status(200).json({
+            status: "success",     
+            items: results.rows,
+        });
+    } catch(err) {
+        console.log('Error found in get method to home/korean-box', err); 
+    }
+});
+
+//to get chinese box and its items to show up on the page, when you click on it!
+app.get('/home/:chinese-box', async (req, res) => {
+    try{
+        const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.chinese-box]);
+        res.status(200).json({
+            status: "success",     
+            items: results.rows,
+        });
+    } catch(err) {
+        console.log('Error found in get method to home/chinese-box', err); 
+    }
+});
+
+//to get mixed box and its items to show up on the page, when you click on it!
+app.get('home/:mixed-box', async (req, res) => {
+    try{
+        const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.mixed-box]);
+        res.status(200).json({
+            status: "success",     
+            items: results.rows,
+        });
+    } catch(err) {
+        console.log('Error found in get method to home/mixed-box', err); 
+    }
+});
+
