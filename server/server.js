3d54abbbc2b5af2ee991efa767ccdfc8802b92d0
ivy118 <47249996+ivy118@users.noreply.github.com>
2021-06-26 17:43:30 -0400

    server IVY

--- server/server.js
+++ server/server.js
@@ -10,9 +10,8 @@ app.use(express.json());
 app.use(express.static('../client/public'));
 
 
-
 //home page: need boxes name and boxes image to render on page!
-app.get('/home', async (req, res) => {
+app.get('/shop', async (req, res) => {
     try{
         const results = await db.query("select * from boxes;");
         res.status(200).json({
@@ -20,76 +19,97 @@ app.get('/home', async (req, res) => {
             boxes: results.rows,
         });
     } catch(err) {
-        console.log('Error found in get method to /home',err); 
+        console.log('Error found in get method to /shop',err); 
     }
 });
 
-//signup page: needs to store user info in database, NEED TO BE FIXED
 
-app.get('/signup', async (req, res, next) => {
+
+//post request to signup page: get the username and email and pass sure they are not in the database yet!
+app.post('/signup', async (req, res, next) => {
     try{
-        const results = await db.query("select * from users WHERE ;");
-        res.status(200).json({
-            status: "success",
-            results: results.rows.length,
-            user: results.rows,
-        });
+        let username = req.body.username;
+        let email = req.body.email;
+
+        const result1 = await db.query('select * from users where username = $1', [username]);
+
+        const result2 = await db.query('select * from users where email = $1', [email]);
+
+        //if both is not in the database, store both
+        if (result1.rows.length === 0 && result2.rows.length === 0) {
+            const results = await db.query("INSERT INTO users (firstname, lastname, username, pass, email) values ($1, $2, $3, $4, $5) returning *;", [req.body.firstname, req.body.lastname, req.body.username, req.body.pass, req.body.email]);
+            res.status(200).redirect('/shop');
+        //something already exists in the database:
+        } else {
+            if (result1.rows.length !== 0 && result2.rows.length !== 0) {
+                res.status(200).json({
+                    data: {
+                        username: false,
+                        email: false
+                    },
+                });
+            }
+            else if (result1.rows.length !== 0) {
+                res.status(200).json({
+                    data: {
+                        username: false,
+                        email: true
+                    },
+                });
+            } else if (result2.rows.length !== 0) {
+                res.status(200).json({
+                    data: {
+                        username: true,
+                        email: false
+                    },
+                });
+            }
+        };
     } catch(err) {
-        console.log(err); 
+        console.log('Error found in post request to /signup', err); 
     }
 });
 
 
-app.post('/signup', async (req, res) => {
+
+//post request to login page: get the username and email and pass sure they are in the database or else they need to sign up!!
+app.post('/login', async (req, res, next) => {
     try{
-        const results = await db.query("INSERT INTO users (firstname, lastname, username, pass, email) values ($1, $2, $3, $4, $5) returning *;", [req.body.firstname, req.body.lastname, req.body.username, req.body.pass, req.body.email]);
-        res.status(200).json({
-            status: "success",     
-            user: results.rows[0],
-        });
+        let username = req.body.username;
+        let password = req.body.pass
+
+        const result1 = await db.query('select * from users where username = $1', [username]);
+
+        const result2 = await db.query('select pass from users where username = $1', [username]);
+
+        //if username is in database, get password to be checked;
+        if (result1.rows.length !== 0) {
+            if (password === result2.rows[0]) {
+                res.status(200).redirect('/shop');
+            } else {
+                res.status(200).json({
+                    data: {username: true,
+                    pass: false}
+                });
+            }
+            //if username is not in database, then user is never signed up, ask user to sign up!
+        } else {
+            res.status(200).json({
+                data: {
+                    username: false,
+                    pass: false
+                },
+            });
+        }
     } catch(err) {
-        console.log('Error found in get method to /signup',err); 
+        console.log('Error found in post request to /login', err); 
     }
 });
 
-
-//login page: need to check that the user's email and password is correct???
-app.get('/login', async (req, res) => {
-    try{
-        const results = await db.query("select * from users;");
-        res.status(200).json({
-            status: "success",
-            results: results.rows.length,
-            data: {
-                restaurant: results.rows,
-            },
-        });
-    } catch(err) {
-        console.log(err); 
-    }
-});
-
-app.get('/restaurant/:id', async (req, res) => {
-    try {
-        const restaurant = await db.query("select * from restaurant where id = $1;", [req.params.id]);
-
-        const reviews = await db.query("select * from review where restaurant_id = $1;", [req.params.id]);
-
-        res.status(200).json({
-            status: "success",
-            data: {
-                restaurant: restaurant.rows[0],
-                reviews: reviews.rows,
-            },
-        });
-    } catch(err) {
-        console.log(err);
-    }
-});
 
 
 //to get japanese box and its items to show up on the page, when you click on it!
-app.get('home/:japanese-box', async (req, res) => {
+app.get('shop/:japanese-box', async (req, res) => {
     try{
         const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.japaness-box]);
         res.status(200).json({
@@ -101,8 +121,10 @@ app.get('home/:japanese-box', async (req, res) => {
     }
 });
 
+
+
 //to get korean box and its items to show up on the page, when you click on it!
-app.get('home/:korean-box', async (req, res) => {
+app.get('shop/:korean-box', async (req, res) => {
     try{
         const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.korean-box]);
         res.status(200).json({
@@ -114,8 +136,10 @@ app.get('home/:korean-box', async (req, res) => {
     }
 });
 
+
+
 //to get chinese box and its items to show up on the page, when you click on it!
-app.get('/home/:chinese-box', async (req, res) => {
+app.get('/shop/:chinese-box', async (req, res) => {
     try{
         const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.chinese-box]);
         res.status(200).json({
@@ -127,8 +151,10 @@ app.get('/home/:chinese-box', async (req, res) => {
     }
 });
 
+
+
 //to get mixed box and its items to show up on the page, when you click on it!
-app.get('home/:mixed-box', async (req, res) => {
+app.get('shop/:mixed-box', async (req, res) => {
     try{
         const results = await db.query("select * from boxes RIGHT JOIN items on boxes.boxID = items.BoxID WHERE boxes.boxName = $1;",[req.params.mixed-box]);
         res.status(200).json({
@@ -140,3 +166,6 @@ app.get('home/:mixed-box', async (req, res) => {
     }
 });
 
+
+
+//to insert into the sales table when user checks out:
