4d95505c8d4444b76e0e799b54fbbaba50c9bba1
jimmycngo <jimmycngo@gmail.com>
2021-06-28 00:56:10 -0700

    added cookies name to add to cart

--- server/index.js
+++ server/index.js
@@ -1,7 +1,31 @@
-const { Pool } = require('pg');
-const pool = new Pool();
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-}
+import React from 'react';
+import express from 'express';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
 
+// ...other imports and Express config
 
+app.get('/*', (req, res) => {
+  const context = {};
+  const app = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+
+  const indexFile = path.resolve('./public/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    );
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`😎 Server is listening on port ${PORT}`);
+});
